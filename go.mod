a7489978a5c71d7c7e3369a4af8d1932d268fc12
Daniel Paul <dp@danielpaul.de>
2023-07-25 18:12:38 +0200

    inital

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/danpaul81/px-rebalance
+
+go 1.19
+
+require (
+	github.com/libopenstorage/openstorage-sdk-clients v0.109.1-0.20230719205218-658caba26bb5
+	google.golang.org/grpc v1.56.2
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto v0.0.0-20230724170836-66ad5b6ff146 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230724170836-66ad5b6ff146 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230724170836-66ad5b6ff146 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"context"
+
+	//"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	//"time"
+
+	api "github.com/libopenstorage/openstorage-sdk-clients/sdk/golang"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+type pxvol struct {
+	id       string
+	repl     int
+	replicas []replicasets
+}
+
+type replicasets struct {
+	node     string
+	az       string
+	attached bool
+}
+
+type pxnode struct {
+	id string
+	az string
+}
+
+var (
+	address   = flag.String("address", "127.0.0.1:9100", "Address to server as <address>:<port>")
+	pxvolumes []pxvol
+)
+
+func main() {
+	ipnodemap := make(map[string]string)
+	nodeazmap := make(map[string]string)
+
+	flag.Parse()
+	dialOptions := []grpc.DialOption{grpc.WithInsecure()}
+
+	conn, err := grpc.Dial(*address, dialOptions...)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+
+	cluster := api.NewOpenStorageClusterClient(conn)
+	// print cluster information
+	clusterInfo, err := cluster.InspectCurrent(
+		context.Background(),
+		&api.SdkClusterInspectCurrentRequest{})
+	if err != nil {
+		gerr, _ := status.FromError(err)
+		fmt.Printf("Error Code[%d] Message[%s]\n",
+			gerr.Code(), gerr.Message())
+		os.Exit(1)
+	}
+	fmt.Printf("Connected to Cluster %s (ID %s)\n",
+		clusterInfo.GetCluster().GetName(), clusterInfo.GetCluster().GetId())
+
+	// collect / print node information
+	nodeclient := api.NewOpenStorageNodeClient(conn)
+	nodeEnumResp, err := nodeclient.Enumerate(
+		context.Background(),
+		&api.SdkNodeEnumerateRequest{})
+	if err != nil {
+		gerr, _ := status.FromError(err)
+		fmt.Printf("Error Code[%d] Message[%s]\n",
+			gerr.Code(), gerr.Message())
+		os.Exit(1)
+	}
+
+	// For each node ID, get its information
+	for _, nodeID := range nodeEnumResp.GetNodeIds() {
+		node, err := nodeclient.Inspect(
+			context.Background(),
+			&api.SdkNodeInspectRequest{
+				NodeId: nodeID,
+			},
+		)
+		if err != nil {
+			gerr, _ := status.FromError(err)
+			fmt.Printf("Error Code[%d] Message[%s]\n",
+				gerr.Code(), gerr.Message())
+			os.Exit(1)
+		}
+		fmt.Printf("Node ID %s \t SchedulerName %s HostIP %s\n", node.GetNode().GetId(), node.GetNode().GetSchedulerNodeName(), node.GetNode().GetDataIp())
+		ipnodemap[node.GetNode().GetDataIp()] = node.GetNode().GetId()
+
+		for key, label := range node.GetNode().GetSchedulerTopology().Labels {
+
+			if key == "topology.portworx.io/zone" {
+				nodeazmap[node.GetNode().GetId()] = label
+			}
+			fmt.Printf("\t %s %s\n", key, label)
+		}
+		//nd, _ := json.MarshalIndent(node, "", "  ")
+		//fmt.Printf(" JSON %s", nd)
+	}
+
+	// print / collect volume information
+	volumeclient := api.NewOpenStorageVolumeClient(conn)
+	volumeEnumResp, err := volumeclient.Enumerate(
+		context.Background(),
+		&api.SdkVolumeEnumerateRequest{})
+	if err != nil {
+		gerr, _ := status.FromError(err)
+		fmt.Printf("Error Code[%d] Message[%s]\n",
+			gerr.Code(), gerr.Message())
+		os.Exit(1)
+	}
+
+	for _, volumeID := range volumeEnumResp.GetVolumeIds() {
+		volume, err := volumeclient.Inspect(
+			context.Background(),
+			&api.SdkVolumeInspectRequest{
+				VolumeId: volumeID,
+			},
+		)
+		if err != nil {
+			gerr, _ := status.FromError(err)
+			fmt.Printf("Error Code[%d] Message[%s]\n",
+				gerr.Code(), gerr.Message())
+			os.Exit(1)
+		}
+
+		// can a replicaset consist of more than 3 nodes? (e.g. stripesets)
+		fmt.Printf("Volume ID %s HA Level %v \n", volume.GetVolume().GetId(), volume.GetVolume().GetSpec().GetHaLevel())
+		mounted := ipnodemap[volume.GetVolume().GetAttachedOn()]
+
+		for _, replicaset := range volume.GetVolume().GetReplicaSets() {
+			for _, replicanodes := range replicaset.Nodes {
+
+				if replicanodes == mounted {
+					fmt.Printf(" \t Node %s AZ %s *mounted \n", replicanodes, nodeazmap[replicanodes])
+				} else {
+					fmt.Printf(" \t Node %s AZ %s \n", replicanodes, nodeazmap[replicanodes])
+				}
+			}
+		}
+
+		//		nd, _ := json.MarshalIndent(volume, "", "  ")
+		//		fmt.Printf(" JSON %s \n", nd)
+		//for _, volumeID := range
+	}
+}
